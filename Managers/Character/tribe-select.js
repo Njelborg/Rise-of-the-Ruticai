353c207f2a6d0d724760998e02b39b57de54679b
Njelborg <128195113+Njelborg@users.noreply.github.com>
2023-04-28 15:35:13 -0500

    Update tribe-select.js

--- Managers/Character/tribe-select.js
+++ Managers/Character/tribe-select.js
@@ -1,11 +1,16 @@
 		// Define a data object to store the options for the third select element
 var tribeData = {
-  "suebia": ["Ruticai", "Rugii", "Sabines", "Lugii", "Erevir", "Havessetts", "Cinucci", "Rus", "Francues", "Angavi", "Bwyngavi", "Bocci", "Averni", "Eruni"],
-  "thracar": ["Obuli", "Ghaeti", "Empire", "Tylani", "Breuki", "Sotioi", "Dracoi", "Bacancci", "Bitiq Nishid"],
-  "ionia": ["Lakonae", "Atiikae", "Boeoti", "Argoli", "Phoki", "Tesselae", "Makkadae", "Euboeoi", "Itikae", "Deloki", "Naxonae", "Ketani", "Gyolidae", "Lokriti"],
-  "cellus": ["Boii", "Sequebri", "Volcae", "Nervaci", "Aedubri", "Helvetani", "Cait", "Fortriu", "Korvanti", "Brigandes", "Demitorix", "Iccitani", "Turdetorix", "Aquitani", "Cantuii", "Lusitani", "Arevaci", "Vangiones","Melliselia"],
-  "sarmatae": ["Palmagate","Durhae","Samuli","Roxotani","Ugyeki","Cimmerian","Vandal","Prusi","Lithuthi","Latvi","Galindian","Dniepr","Polushi","Sicae","Croati","Pomeri","Morov","Czechae","Obo Trites","Imperial Vevuti","Slovaki","Vistuli","Bohemae"],
-  "illatium": ["Novus","Tarchuna","Lyrunus","Felathri","Samnus","Ventecci","Aucai","Brutii","Frentani","Lucani","Marrucini","Umbri","Altucci","Senolni","Churbes","Syracus","Tara","Igioli","Bulari","Corci"]
+  "--Select--": ["--Select--"],
+  "suebia": ["--Select--", "Ruticai", "Rugii", "Sabines", "Lugii", "Erevir", "Havessetts", "Cinucci", "Rus", "Francues", "Angavi", "Bwyngavi", "Bocci", "Averni", "Eruni"],
+  "thracar": ["--Select--", "Obuli", "Ghaeti", "Empire", "Tylani", "Breuki", "Sotioi", "Dracoi", "Bacancci", "Bitiq Nishid"],
+  "ionia": ["--Select--", "Lakonae", "Atiikae", "Boeoti", "Argoli", "Phoki", "Tesselae", "Makkadae", "Euboeoi", "Itikae", "Deloki", "Naxonae", "Ketani", "Gyolidae", "Lokriti"],
+  "cellus": ["--Select--", "Boii", "Sequebri", "Volcae", "Nervaci", "Aedubri", "Helvetani", "Cait", "Fortriu", "Korvanti", "Brigandes", "Demitorix", "Iccitani", "Turdetorix", "Aquitani", "Cantuii", "Lusitani", "Arevaci", "Vangiones","Melliselia"],
+  "sarmatae": ["--Select--", "Palmagate","Durhae","Samuli","Roxotani","Ugyeki","Cimmerian","Vandal","Prusi","Lithuthi","Latvi","Galindian","Dniepr","Polushi","Sicae","Croati","Pomeri","Morov","Czechae","Obo Trites","Imperial Vevuti","Slovaki","Vistuli","Bohemae"],
+  "illatium": ["--Select--", "Novus","Tarchuna","Lyrunus","Felathri","Samnus","Ventecci","Aucai","Brutii","Frentani","Lucani","Marrucini","Umbri","Altucci","Senolni","Churbes","Syracus","Tara","Igioli","Bulari","Corci"],
+  "shim": ["--Select--"],
+  "libue": ["--Select--"],
+  "croesios": ["--Select--"],
+  
   // Add the rest of the options here
 };
 

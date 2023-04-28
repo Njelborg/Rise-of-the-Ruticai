29012004fa743d96a71c717f6866fa649bc1f16e
Njelborg <128195113+Njelborg@users.noreply.github.com>
2023-04-28 15:36:26 -0500

    Update realm-skill-select.js

--- Managers/Character/realm-skill-select.js
+++ Managers/Character/realm-skill-select.js
@@ -1,20 +1,21 @@
 // Define a data object to store the options for the second select element
 var data = {
-  "cellus": ["Melee & Stealth", "Melee & Riding", "Stealth & Riding"],
-  "croesios": ["Melee & Acting", "Melee & Social Skills", "Acting & Social Skills"],
-  "illatium": ["Crafts & Melee", "Crafts & Stamina", "Melee & Stamina"],
-  "ionia": ["Melee & Seamanship", "Melee & World Lore", "Seamanship & World Lore"],
-  "libue": ["Acting & Climbing", "Acting & Stamina", "Climbing & Stamina"],
-  "sarmatae": ["Missile & Riding", "Missile & Trickery", "Riding & Trickery"],
-  "shim": ["Missile & Extra Language", "Missile & Social Skills", "Extra Language & Social Skills"],
-  "suebia": ["Melee & Tracking", "Melee & Navigation", "Tracking & Navigation"],
-  "thracar": ["Missile & Stamina", "Missile & Stealth", "Stamina & Stealth"]
+  "cellus": ["--Select--", "Stealth", "Riding"],
+  "croesios": ["--Select--", "Acting", "Social Skills"],
+  "illatium": ["--Select--", "Crafts", "Stamina"],
+  "ionia": ["--Select--", "Seamanship", "World Lore"],
+  "libue": ["--Select--", "Climbing", "Stamina"],
+  "sarmatae": ["--Select--", "Riding", "Trickery"],
+  "shim": ["--Select--", "Extra Language", "Social Skills"],
+  "suebia": ["--Select--", "Tracking", "Navigation"],
+  "thracar": ["--Select--", "Stamina", "Stealth"]
 };
 
 // Get references to the select elements
 var genderSelect = document.getElementById("gender");
-var firstSelect = document.getElementById("realm");
-var secondSelect = document.getElementById("realm-skill");
+var realmSelect = document.getElementById("realm");
+var realmSkill1Select = document.getElementById("realm-skill-1");
+var realmSkill2Select = document.getElementById("realm-skill-2");
 
 // Add a change event listener to the gender select element
 genderSelect.addEventListener("change", function() {
@@ -22,44 +23,64 @@ genderSelect.addEventListener("change", function() {
   var selectedGender = this.value;
   // If the selected value is male, enable the second select element and its options
   if (selectedGender == "male") {
-    secondSelect.disabled = false;
-    var options = secondSelect.options;
-    for (var i = 0; i < options.length; i++) {
-      options[i].disabled = false;
+    realmSkill1Select.disabled = false;
+    realmSkill2Select.disabled = false;
+    var options1 = realmSkill1Select.options;
+    var options2 = realmSkill2Select.options;
+    for (var i = 0; i < options1.length; i++) {
+      options1[i].disabled = false;
+    }
+    for (var i = 0; i < options2.length; i++) {
+      options2[i].disabled = false;
     }
     // Trigger the change event of the first select element to update the second select element
-    firstSelect.dispatchEvent(new Event("change"));
+    realmSelect.dispatchEvent(new Event("change"));
   }
-  // Otherwise, disable the second select element and its options, and set it to N/A
+  // Otherwise, disable the second select elements and their options, and set them to N/A
   else {
-    secondSelect.disabled = true;
-    secondSelect.innerHTML = "";
-    var option = document.createElement("option");
-    option.value = "N/A";
-    option.text = "N/A";
-    option.selected = true;
-    secondSelect.appendChild(option);
+    realmSkill1Select.disabled = true;
+    realmSkill1Select.innerHTML = "";
+    var option1 = document.createElement("option");
+    option1.value = "N/A";
+    option1.text = "N/A";
+    option1.selected = true;
+    realmSkill1Select.appendChild(option1);
+    realmSkill2Select.disabled = true;
+    realmSkill2Select.innerHTML = "";
+    var option2 = document.createElement("option");
+    option2.value = "N/A";
+    option2.text = "N/A";
+    option2.selected = true;
+    realmSkill2Select.appendChild(option2);
   }
 });
 
-// Add a change event listener to the first select element
-firstSelect.addEventListener("change", function() {
+// Add a change event listener to the realm select element
+realmSelect.addEventListener("change", function() {
   // Get the selected value of the gender select element
   var selectedGender = genderSelect.value;
-  // If the selected value is male, update the second select element based on the first select element
+  // If the selected value is male, update the select elements based on the first select element
   if (selectedGender == "male") {
-    // Clear the options of the second select element
-    secondSelect.innerHTML = "";
-    // Get the selected value of the first select element
+    // Clear the options of the realm skill select elements
+    realmSkill1Select.innerHTML = "";
+    realmSkill2Select.innerHTML = "";
+    // Get the selected value of the realm select element
     var selectedValue = this.value;
     // Get the corresponding options from the data object
     var options = data[selectedValue];
-    // Add options to the second select element
+    // Add options to the realm skill 2 select element
     for (var i = 0; i < options.length; i++) {
       var option = document.createElement("option");
       option.value = options[i];
       option.text = options[i];
-      secondSelect.appendChild(option);
+      realmSkill2Select.appendChild(option);
     }
+    // Set the value of the realm skill 1 select element based on the realm
+    var realmSkill1Value = (selectedValue == "cellus" || selectedValue == "croesios" || selectedValue == "illatium" || selectedValue == "ionia" || selectedValue == "suebia") ? "Melee" : "Missile";
+    realmSkill1Select.value = realmSkill1Value;
+
   }
 });
+
+// Trigger the change event of the gender select element to initialize the realm skill select elements
+genderSelect.dispatchEvent(new Event("change"));

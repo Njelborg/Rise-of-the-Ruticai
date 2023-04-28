7e9c878ce72e3132db630f5a6d24daa1d060c198
Njelborg <128195113+Njelborg@users.noreply.github.com>
2023-04-28 15:38:55 -0500

    Update favorite-deity-select.js

--- Managers/Character/favorite-deity-select.js
+++ Managers/Character/favorite-deity-select.js
@@ -5,15 +5,15 @@ var favoriteDeity = document.getElementById("favorite-deity");
 
 // Define an object that maps each realm to an array of deities
 var deitiesByRealm = {
-  cellus: ["--Select--" "UnaR", "Wotan", "Freya", "Herni", "The Horned Goddess", "BalthuR", "Erthi", "Hadr", "TyranaR"],
-  croesios: ["--Select--" "Tiri & Tishtyra", "Mithra", "Hvar Ksata", "Bumbo", "Shango", "Baal", "Melqart", "Isis", "Nephtys"],
-  illatium: ["--Select--" "Ephasos", "Athullos", "Violios", "Satanas", "Bacchus", "Milas", "Mertinas", "Epocchas", "Aphras"],
-  ionia: ["--Select--" "Ephasos", "Athullos", "Violios", "Satanas", "Bacchus", "Milas", "Mertinas", "Epocchas", "Aphras"],
-  libue: ["--Select--" "Tiri & Tishtyra", "Mithra", "Hvar Ksata", "Bumbo", "Shango", "Baal", "Melqart", "Isis", "Nephtys"],
-  sarmatae: ["--Select--" "UnaR", "Wotan", "Freya", "Herni", "The Horned Goddess", "BalthuR", "Erthi", "Hadr", "TyranaR"],
-  shim: ["--Select--" "Tiri & Tishtyra", "Mithra", "Hvar Ksata", "Bumbo", "Shango", "Baal", "Melqart", "Isis", "Nephtys"],
-  suebia: ["--Select--" "UnaR", "Wotan", "Freya", "Herni", "The Horned Goddess", "BalthuR","Erthi","Hadr","TyranaR"],
-  thracar: ["--Select--" "Ephasos","Athullos","Violios","Satanas","Bacchus","Milas","Mertinas","Epocchas","Aphras"]
+  cellus: ["--Select--", "UnaR", "Wotan", "Freya", "Herni", "The Horned Goddess", "BalthuR", "Erthi", "Hadr", "TyranaR"],
+  croesios: ["--Select--", "Tiri & Tishtyra", "Mithra", "Hvar Ksata", "Bumbo", "Shango", "Baal", "Melqart", "Isis", "Nephtys"],
+  illatium: ["--Select--", "Ephasos", "Athullos", "Violios", "Satanas", "Bacchus", "Milas", "Mertinas", "Epocchas", "Aphras"],
+  ionia: ["--Select--", "Ephasos", "Athullos", "Violios", "Satanas", "Bacchus", "Milas", "Mertinas", "Epocchas", "Aphras"],
+  libue: ["--Select--", "Tiri & Tishtyra", "Mithra", "Hvar Ksata", "Bumbo", "Shango", "Baal", "Melqart", "Isis", "Nephtys"],
+  sarmatae: ["--Select--", "UnaR", "Wotan", "Freya", "Herni", "The Horned Goddess", "BalthuR", "Erthi", "Hadr", "TyranaR"],
+  shim: ["--Select--", "Tiri & Tishtyra", "Mithra", "Hvar Ksata", "Bumbo", "Shango", "Baal", "Melqart", "Isis", "Nephtys"],
+  suebia: ["--Select--", "UnaR", "Wotan", "Freya", "Herni", "The Horned Goddess", "BalthuR","Erthi","Hadr","TyranaR"],
+  thracar: ["--Select--", "Ephasos","Athullos","Violios","Satanas","Bacchus","Milas","Mertinas","Epocchas","Aphras"]
 };
 
 // Define a function that updates the favorite deity options based on the realm value

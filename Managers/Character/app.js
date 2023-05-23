71615dca0184f2bb99d4ad0cd8808c4cd4d9469f
Njelborg <128195113+Njelborg@users.noreply.github.com>
2023-05-23 12:27:18 -0500

    Update app.js

--- Managers/Character/app.js
+++ Managers/Character/app.js
@@ -1,24 +1,50 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const tabs = document.querySelectorAll('#tabs button');
-    const content = document.querySelectorAll('.tab-content');
+// Get the modal
+var modal = document.getElementById("myModal");
 
-    function switchTab(e) {
-        content.forEach((el) => {
-            el.classList.remove('active');
-        });
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-        tabs.forEach((tab) => {
-            tab.classList.remove('active');
-        });
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-        document.getElementById(e.target.id.replace('-tab', '')).classList.add('active');
-        e.target.classList.add('active');
+// Get the form element
+var form = document.getElementById("myForm");
+
+// Get the save and cancel buttons
+var saveBtn = document.getElementById("saveBtn");
+var cancelBtn = document.getElementById("cancelBtn");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.showModal();
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.close();
+}
+
+// When the user clicks on save button, submit the form and close the modal
+saveBtn.onclick = function(e) {
+  e.preventDefault(); // prevent the form from being submitted normally
+
+  var name = $("#name").val();
+
+  $.ajax({
+    url: "save_character.php", // replace this with the actual path to your PHP script
+    type: "POST",
+    data: {name: name},
+    success: function(response) {
+      alert(response); // handle the response from the PHP script
+      modal.close(); // close the modal
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+       console.log(textStatus, errorThrown);
     }
+  });
+}
 
-    tabs.forEach((tab) => {
-        tab.addEventListener('click', switchTab);
-    });
-
-    // Set the first tab as active by default
-    document.getElementById('info-tab').click();
-});
+// When the user clicks on cancel button, close the modal
+cancelBtn.onclick = function() {
+  modal.close();
+}

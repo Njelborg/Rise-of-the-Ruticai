c73386e183ac4d5982e63e249bcf11ce6b27d672
Njelborg <128195113+Njelborg@users.noreply.github.com>
2023-05-23 12:39:16 -0500

    Update app.js

--- Managers/Character/app.js
+++ Managers/Character/app.js
@@ -1,50 +1,29 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+document.addEventListener('DOMContentLoaded', () => {
+  const createSelectBtn = document.getElementById('create-select');
+  const createForm = document.getElementById('create-form');
+  const createInventoryBtn = document.getElementById('inventory');
+  const inventoryForm = document.getElementById('inventory-form');
+  const createCharacterBtn = document.getElementById('character-sheet');
+  const characterForm = document.getElementById('character-form');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// Get the form element
-var form = document.getElementById("myForm");
-
-// Get the save and cancel buttons
-var saveBtn = document.getElementById("saveBtn");
-var cancelBtn = document.getElementById("cancelBtn");
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.showModal();
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.close();
-}
-
-// When the user clicks on save button, submit the form and close the modal
-saveBtn.onclick = function(e) {
-  e.preventDefault(); // prevent the form from being submitted normally
-
-  var name = $("#name").val();
-
-  $.ajax({
-    url: "save_character.php", // replace this with the actual path to your PHP script
-    type: "POST",
-    data: {name: name},
-    success: function(response) {
-      alert(response); // handle the response from the PHP script
-      modal.close(); // close the modal
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-       console.log(textStatus, errorThrown);
-    }
+  createSelectBtn.addEventListener('click', () => {
+    hideAllForms();
+    createForm.style.display = 'block';
   });
-}
 
-// When the user clicks on cancel button, close the modal
-cancelBtn.onclick = function() {
-  modal.close();
-}
+  createInventoryBtn.addEventListener('click', () => {
+    hideAllForms();
+    inventoryForm.style.display = 'block';
+  });
+
+  createCharacterBtn.addEventListener('click', () => {
+    hideAllForms();
+    characterForm.style.display = 'block';
+  });
+
+  function hideAllForms() {
+    createForm.style.display = 'none';
+    inventoryForm.style.display = 'none';
+    characterForm.style.display = 'none';
+  }
+});
